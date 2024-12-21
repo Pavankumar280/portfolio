5dc0b62f447ef1c2fe3a3b2cf45fa82090ebf7b2
pavan <sonune984@gmail.com>
2024-12-21 21:49:01 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Pavankumar Sonune</span> currently a final-year Computer Science and Engineering undergraduate student at Prof. Ram Meghe Institute of Technology and Research, Amravati. I have a strong foundation in web development technologies, including HTML, CSS, and JavaScript, along with hands-on experience in data structures and algorithms (DSA) problem-solving. I am proficient in programming languages such as Java, C, and Python. I am seeking an entry-level position where I can apply my skills to develop and maintain high-quality websites."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
